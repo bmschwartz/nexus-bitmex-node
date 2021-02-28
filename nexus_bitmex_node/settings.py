eb71a0a5ecbf64e739f5bf4801db28d9ec62f407
Ben Schwartz <schwartz.ben0@gmail.com>
2021-02-27 22:40:29 -0700

    Fix settings. Update requirements.txt

--- nexus_bitmex_node/settings.py
+++ nexus_bitmex_node/settings.py
@@ -4,9 +4,10 @@ from enum import Enum
 import logging
 
 from starlette.config import Config
+from starlette.datastructures import CommaSeparatedStrings
 
 from nexus_bitmex_node.ssm_parameter_store import SSMParameterStore
-from nexus_bitmex_node.logger import LogLevel
+from nexus_bitmex_node.logger import LogLevel, LoggingFormat, generate_logging_config
 
 
 class ServerMode(Enum):
@@ -60,31 +61,31 @@ time.sleep(0.5)
 LOG_LEVEL_ENUM = config("LOG_LEVEL", cast=LogLevel, default=LogLevel.INFO)
 LOG_LEVEL = LOG_LEVEL_ENUM.value
 IS_DEBUG = LOG_LEVEL_ENUM == LogLevel.DEBUG
-# DEBUG_LOGGERS = config("DEBUG_LOGGERS", cast=CommaSeparatedStrings, default=[])
-# WARNING_LOGGERS = config("WARNING_LOGGERS", cast=CommaSeparatedStrings, default=[])
-# INFO_LOGGERS = config("INFO_LOGGERS", cast=CommaSeparatedStrings, default=[])
+DEBUG_LOGGERS = config("DEBUG_LOGGERS", cast=CommaSeparatedStrings, default=[])
+WARNING_LOGGERS = config("WARNING_LOGGERS", cast=CommaSeparatedStrings, default=[])
+INFO_LOGGERS = config("INFO_LOGGERS", cast=CommaSeparatedStrings, default=[])
 SERVER_MODE = config("SERVER_MODE", cast=ServerMode, default=ServerMode.DEV)
-# LOGGING_FORMAT = config(
-#     "LOGGING_FORMAT", cast=LoggingFormat, default=LoggingFormat.DEFAULT
-# )
-#
-# if SERVER_MODE not in [ServerMode.STAGING, ServerMode.DEMO, ServerMode.PROD]:
-#     LOGGING_CONFIG = generate_logging_config(
-#         APP_NAME,
-#         level=serialize_log_level(LOG_LEVEL_ENUM),
-#         log_handler="logging.StreamHandler",
-#         formatter=LOGGING_FORMAT.value,
-#         debug_loggers=DEBUG_LOGGERS,
-#         warning_loggers=WARNING_LOGGERS,
-#         info_loggers=INFO_LOGGERS,
-#     )
-# else:
-#     LOGGING_CONFIG = generate_logging_config(
-#         APP_NAME,
-#         level=serialize_log_level(LOG_LEVEL_ENUM),
-#         debug_loggers=DEBUG_LOGGERS,
-#         warning_loggers=WARNING_LOGGERS,
-#         info_loggers=INFO_LOGGERS,
-#     )
-#
+LOGGING_FORMAT = config(
+    "LOGGING_FORMAT", cast=LoggingFormat, default=LoggingFormat.DEFAULT
+)
+
+if SERVER_MODE not in [ServerMode.STAGING, ServerMode.DEMO, ServerMode.PROD]:
+    LOGGING_CONFIG = generate_logging_config(
+        APP_NAME,
+        level=serialize_log_level(LOG_LEVEL_ENUM),
+        log_handler="logging.StreamHandler",
+        formatter=LOGGING_FORMAT.value,
+        debug_loggers=DEBUG_LOGGERS,
+        warning_loggers=WARNING_LOGGERS,
+        info_loggers=INFO_LOGGERS,
+    )
+else:
+    LOGGING_CONFIG = generate_logging_config(
+        APP_NAME,
+        level=serialize_log_level(LOG_LEVEL_ENUM),
+        debug_loggers=DEBUG_LOGGERS,
+        warning_loggers=WARNING_LOGGERS,
+        info_loggers=INFO_LOGGERS,
+    )
+
 # dictConfig(LOGGING_CONFIG)
